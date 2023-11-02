59fe816e4f3e0fac8e53ccbb02404feefaa80acc
hasan <mmhotmail>
2023-11-02 14:56:16 +0300

    product

--- src/routes/index.js
+++ src/routes/index.js
@@ -21,9 +21,9 @@ router.use('/brands', require('./brand'))
 // // category:
 router.use('/categories', require('./category'))
 // // firm:
-// router.use('/firms', require('./firm'))
+router.use('/firms', require('./firm'))
 // // product:
-// router.use('/products', require('./product'))
+router.use('/products', require('./product'))
 // // purchase:
 // router.use('/purchases', require('./purchase'))
 // // sale:
